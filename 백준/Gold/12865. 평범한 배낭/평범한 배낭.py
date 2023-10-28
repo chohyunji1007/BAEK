ac7dbf959b437efb5bd3a702c553fc22e54c6beb
chohyunji1007 <145529997+chohyunji1007@users.noreply.github.com>
2023-10-28 14:19:51 +0900

    [Gold V] Title: 평범한 배낭, Time: 456 ms, Memory: 191016 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -11,7 +11,7 @@ for i in range(1,n+1):
     for j in range(1,k+1):
         if j>=bag_list[i-1][0]: #j가 현재 배낭 무게보다 큰 경우
             if bag[i-1][j-bag_list[i-1][0]]!=0: #0이 아니면!
-                input_w = max(bag_list[i-1][1], bag[i-1][j], bag[i-1][j-bag_list[i-1][0]]+bag_list[i-1][1])
+                input_w = max(bag[i-1][j], bag[i-1][j-bag_list[i-1][0]]+bag_list[i-1][1])
                 bag[i][j] = input_w
             else: #0이면
                 bag[i][j]=max(bag_list[i-1][1], bag[i-1][j])
