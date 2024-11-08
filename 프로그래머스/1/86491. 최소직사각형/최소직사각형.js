f2191854f5af30cac7d9df30a6248492673abdcf
chohyunji1007 <145529997+chohyunji1007@users.noreply.github.com>
2024-11-08 09:50:38 +0900

    [level 1] Title: 최소직사각형, Time: 1.41 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -2,12 +2,24 @@ function solution(sizes) {
     var answer = 0;
     var w = 0;
     var h = 0;
-    for(var i=0; i<sizes.length; i++){
-        sizes[i].sort((a, b)=>{return b-a;});
-        w<sizes[i][0] ? w=sizes[i][0] : null ;
-        h<sizes[i][1] ? h=sizes[i][1] : null ;
-    }
     
-    answer = w*h;
-    return answer;
+    // console.log(sizes);
+    
+    sizes.forEach((element, index, array)=>{
+         // console.log(element, array);
+        if(element[0]>element[1]){
+            var c = element[0];
+            element[0] = element[1];
+            element[1] = c;
+        }
+        if(w < element[0]){
+            w = element[0];
+        }
+        if(h < element[1]){
+            h = element[1];
+        }
+    });
+    // console.log(w, h);
+    
+    return w*h;
 }
