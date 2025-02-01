6e754a6840392f0a0906ee6fcb8c233133c5db27
chohyunji1007 <145529997+chohyunji1007@users.noreply.github.com>
2025-02-01 14:40:37 +0900

    [level 0] Title: 옹알이 (1), Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,38 @@
+function solution(babbling) {
+    var answer = 0;
+    // aya , ye , woo , ma 4가지만 가능
+    
+    
+    var a = ["aya", "ye", "woo", "ma", ""];
+    var ar = make(a);
+    for(const entry of babbling){
+        if(ar.includes(entry)){ 
+            // console.log(entry);
+            answer++;
+        }
+    }
+    
+    return answer;
+}
+function make(a){
+    var arr = a.filter((data)=>data!=='');
+    var len = a.length;
+    for(var i=0; i<len; i++){
+        var fir = a[i];
+        for(var j=0; j<len; j++){
+            var second = fir + a[j];
+            arr.push(second);
+            for(var k=0; k<len; k++){
+                var th = second + a[k];
+                arr.push(th);
+                for(var q=0; q<len; q++){
+                    var fo = th+a[q];
+                    arr.push(fo);
+                }
+            }
+        }
+    }
+    arr = [... new Set(arr)];
+    return arr;
+    // console.log(arr);
+}
